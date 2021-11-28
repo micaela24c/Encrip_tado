c7e0ba3c3cb8d8eff8a74929ade3c40c5ca57fd0
Micaela <94004537+micaela24c@users.noreply.github.com>
2021-11-28 09:43:56 -0500

    m

--- assets/js/encriptado.js
+++ assets/js/encriptado.js
@@ -73,25 +73,51 @@ function desencriptar(elemento){
     var base64Frase = document.getElementsByTagName("input")[0].value;
     var palabra_descodificada = "";
 
-    for (let j = 0; j < Math.floor(binString.length / 6); j++) { // CORREGIDO
-        bytes6 = "00" + binString.slice(j, j + 6);
-        newAscii = parseInt(bytes6, 2);
-        newbase64 += base64[newAscii];
-    }
-    
-    let cerosDerecha = 6 - (binString.length % 6); // CORREGIDO
-    let subBinString = "00" + binString.slice((Math.floor(binString.length / 6) * 6)); // CORREGIDO
-    for (let k = 0; k < cerosDerecha; k++) {
-        subBinString += "0";
-    }
-    newAscii = parseInt(subBinString, 2);
-    newbase64 += base64[newAscii];
-    for (let l = 0; l < cerosDerecha; l += 2) {
-        newbase64 += "=";
-    }
+    for (let i = 0; i < palabra.length; i += 3) {
+        let subPalabra = palabra.slice(i, i + 3);
+        let binChar = "";
+        let binString = "";
+        let nCerosIzquierda = 0; // número ceros a la izquierda
+        for (let j = 0; j < subPalabra.length; j++) {
+            let asciiCode = subPalabra.charCodeAt(j);
+            binChar = asciiCode.toString(2);
+            nCerosIzquierda = 8 - (binChar.length % 8); // AGREGADO: Algunas veces el número binario tiene 6 dígitos y otras veces 7
+            
+            let cerosIzquierda = "";
+            for (let k = 0; k < nCerosIzquierda; k++) {
+                cerosIzquierda += "0";
+            }
+            binString += cerosIzquierda + binChar;
+        }
 
-     
-    palabra_descodificada += newbase64;
-
+        let bytes6 = "";
+        let newAscii = "";
+        var newbase64 = "";
+        if (binString.length % 6 == 0) {  // CORREGIDO
+            for (let k = 0; k < binString.length; k += 6) {
+                bytes6 = "00" + binString.slice(k, k + 6);
+                newAscii = parseInt(bytes6, 2);
+                newbase64 += base64[newAscii];
+            }
+        } else {
+            for (let j = 0; j < Math.floor(binString.length / 6); j++) { // CORREGIDO
+                bytes6 = "00" + binString.slice(j, j + 6);
+                newAscii = parseInt(bytes6, 2);
+                newbase64 += base64[newAscii];
+            }
+            
+            let cerosDerecha = 6 - (binString.length % 6); // CORREGIDO
+            let subBinString = "00" + binString.slice((Math.floor(binString.length / 6) * 6)); // CORREGIDO
+            for (let k = 0; k < cerosDerecha; k++) {
+                subBinString += "0";
+            }
+            newAscii = parseInt(subBinString, 2);
+            newbase64 += base64[newAscii];
+            for (let l = 0; l < cerosDerecha; l += 2) {
+                newbase64 += "=";
+            }
+        }
+        base64Frase += newbase64;
+    }
     document.getElementById("resultado").innerHTML = palabra_descodificada;
 }
